77a8c7029be59efc2f44e6db8b25517129bc47e0
Mirroxum <director.kernelix@yandex.ru>
2022-11-18 15:04:36 +1000

    v2.1

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,24 +1,39 @@
 from django.contrib.admin import register
 from django.contrib.auth.admin import UserAdmin
 
+from .forms import UserChangeForm, UserCreationForm
 from .models import User
 from foodgram.conf import EMPTY_VALUE
 
 
 @register(User)
 class UserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
     list_display = (
-        'username', 'first_name', 'last_name', 'email',
+        'id', 'email', 'username', 'first_name', 'last_name', 'is_blocked',
+        'is_superuser',
     )
-
-    fields = (
-        ('username', 'email',),
-        ('first_name', 'last_name', ),
-        ('subscribe',),
+    list_filter = (
+        'email', 'username', 'is_blocked', 'is_superuser',
     )
-    fieldsets = []
-    search_fields = (
-        'username', 'email',
+    fieldsets = (
+        (None, {'fields': (
+            'email', 'username', 'first_name', 'last_name', 'password',
+        )}),
+        ('Permissions', {'fields': ('is_blocked', 'is_superuser',)})
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'username', 'first_name', 'last_name', 'password1',
+                'password2', 'is_blocked', 'is_superuser',
+            )
+        }),
+    )
+    search_fields = ('email', 'username', 'first_name', 'last_name',)
+    ordering = ('id', 'email', 'username',)
     list_filter = ('username', 'email')
     empty_value_display = EMPTY_VALUE
