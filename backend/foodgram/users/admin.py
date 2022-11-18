2130f92ca2567f1389bad383738d974c0d92a4fa
Mirroxum <director.kernelix@yandex.ru>
2022-11-18 14:49:23 +1000

    v2.1

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -18,7 +18,7 @@ class UserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),}),)
+            'fields': ('email', 'password1', 'password2'), }),)
     search_fields = (
         'username', 'email',
     )
