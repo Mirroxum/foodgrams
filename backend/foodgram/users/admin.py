70c3fa3517002eb748fe196f0f8ac37094ae0a9d
Mirroxum <director.kernelix@yandex.ru>
2022-11-18 14:42:36 +1000

    v2.1

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,22 +1,24 @@
-from django.contrib.admin import register, ModelAdmin
+from django.contrib.admin import register
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 from foodgram.conf import EMPTY_VALUE
 
 
 @register(User)
-class UserAdmin(ModelAdmin):
+class UserAdmin(UserAdmin):
     list_display = (
         'username', 'first_name', 'last_name', 'email',
     )
-
-    fields = (
-        ('username', 'email',),
-        ('first_name', 'last_name',),
-        ('password'),
-        ('subscribe',),
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Subscribe', {'fields': ('subscribe')}),
     )
-    fieldsets = []
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),}),)
     search_fields = (
         'username', 'email',
     )
