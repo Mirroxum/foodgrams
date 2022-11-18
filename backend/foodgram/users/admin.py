5ef4cca85f036bbf16e351e1160df2b2f76f7a99
Mirroxum <director.kernelix@yandex.ru>
2022-11-18 13:49:53 +1000

    v2.1

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -13,7 +13,7 @@ class UserAdmin(ModelAdmin):
     fields = (
         ('username', 'email',),
         ('first_name', 'last_name',),
-        ('password1', 'password2',),
+        ('password'),
         ('subscribe',),
     )
     fieldsets = []
